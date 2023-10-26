a68ec00c0492f26b50771698b43d2827b8947fbd
Jo Young <githuborigin@gmail.com>
2023-10-26 10:04:38 +0900

    feat:공용 컬러 파일을 제작하여 앱 테마 통합관리 준비

---
+++ ace/lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+// 컬러 사전 지정
+// 사용 예시
+// Container(
+//   color: primaryColor,
+// )
+
+  static const primaryColor = Color(0xFF007BFF);
+  static const secondaryColor = Color(0xFF6C757D);
+}
