27226577c58d1bdd90d5df9cbd9ab5180451503f
jung Won hyeong <jeongwon264@gmail.com>
2023-10-27 14:57:48 +0900

    fix:컬러

--- ace/lib/utils/colors.dart
+++ ace/lib/utils/colors.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Colors {
+  const Colors();
 // 컬러 사전 지정
 // 사용 예시
 // Container(
 //   color: primaryColor,
 // )
 
-  static const primaryColor = Color(0xFF007BFF);
-  static const secondaryColor = Color(0xFF6C757D);
-  static const statusColor = Color(0xFF6C757D);
-
+  static const Color primaryColor = Color(0xFF007BFF);
+  static const Color secondaryColor = Color(0xFF6C757D);
+  static const Color statusColor = Color(0xFF6C757D);
 }
