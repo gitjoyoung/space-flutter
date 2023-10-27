7abf1ede8f252384788fe212f5e74c0f84394f26
Jo Young <githuborigin@gmail.com>
2023-10-27 13:02:16 +0900

    feat:api route 예시

---
+++ ace/lib/routes/api_route.dart
@@ -0,0 +1,6 @@
+class ApiRoute {
+  static const String baseUrl = 'https://dev.sniperfactory.com/';
+  static const String login = "${baseUrl}api/auth/login";
+  static const String signup = "${baseUrl}api/auth/signup";
+  
+}
