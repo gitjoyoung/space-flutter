3e8468ce8a94f3f67df0ff91afea0d06508baa4a
Jo Young <githuborigin@gmail.com>
2023-10-29 06:18:39 +0900

    fix:로그인페이지 디자인 및 버튼 비활성화 추가

--- ace/lib/views/authpage/login.dart
+++ ace/lib/views/authpage/login.dart
@@ -1,5 +1,11 @@
+import 'package:ace/controller/auth_controller.dart';
+import 'package:ace/controller/find_password_controller.dart';
+import 'package:ace/controller/login_controller.dart';
+import 'package:ace/routes/view_route.dart';
 import 'package:ace/utils/button.dart';
 import 'package:ace/utils/typography.dart';
+import 'package:ace/views/authpage/find_password.dart';
+import 'package:ace/views/authpage/registration_page.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -9,13 +15,28 @@ import '../../utils/colors.dart';
 import '../../widgets/text_filed_custom.dart';
 
 void main() {
-  runApp(const GetMaterialApp(home: LoginView()));
+  Get.put(LoginController());
+  Get.put(AuthController());
+  Get.put(FindPasswordController());
+
+  runApp(
+    GetMaterialApp(
+      home: LoginView(),
+      getPages: [
+        GetPage(name: ViewRoute.loginPage, page: () => LoginView()),
+        GetPage(name: ViewRoute.findPassword, page: () => FindPassword()),
+        GetPage(
+            name: ViewRoute.registrationPage,
+            page: () => RegistrationView()), // 이 부분을 추가
+        // 이 부분을 추가
+        // 다른 라우트들
+      ],
+    ),
+  );
 }
 
-class LoginView extends GetView {
+class LoginView extends GetView<LoginController> {
   const LoginView({super.key});
-  static const String route = '/login';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,25 +58,29 @@ class LoginView extends GetView {
                     child: TextFormFieldCustom(
                         hintText: '이메일 주소를 입력해주세요.',
                         errorText: '이메일 주소가 틀립니다. 다시 한번 입력해주세요.',
-                        controller: TextEditingController()),
+                        controller: controller.email),
                   ),
                   Container(
                     height: 66,
                     child: TextFormFieldCustom(
                         hintText: '비밀번호를 입력해주세요.',
                         errorText: '비밀번호가 틀립니다. 다시 한번 입력해주세요.',
-                        controller: TextEditingController()),
+                        password: true,
+                        controller: controller.password),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: RichText(
                       textAlign: TextAlign.center,
                       text: TextSpan(
-                        style: const TextStyle(color: AppColors.neutral60),
+                        style: AppTypograpy.button36Medium
+                            .copyWith(color: AppColors.neutral50),
+                        // style: const TextStyle(color: AppColors.neutral60),
                         children: [
                           TextSpan(
                             recognizer: TapGestureRecognizer()
                               ..onTap = () {
+                                Get.toNamed(ViewRoute.findPassword);
                                 print('비밀번호 찾기 클릭');
                               },
                             text: '비밀번호 찾기',
@@ -67,6 +92,8 @@ class LoginView extends GetView {
                             text: '회원가입하기',
                             recognizer: TapGestureRecognizer()
                               ..onTap = () {
+                                Get.toNamed(ViewRoute.registrationPage);
+
                                 print('회원가입하기 클릭');
                               },
                           ),
@@ -77,11 +104,20 @@ class LoginView extends GetView {
                   const SizedBox(
                     height: 120,
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('로그인'),
-                    style: AppButton.xLarge,
-                  )
+                  Obx(() {
+                    return ElevatedButton(
+                      onPressed: controller.isButtonEnabled.value
+                          ? () {
+                              controller.login();
+                            }
+                          : null,
+                      child: const Text(
+                        '로그인',
+                        style: AppTypograpy.tapButtonMedium18,
+                      ),
+                      style: AppButton.xLarge,
+                    );
+                  })
                 ]),
           ),
         ),
