296ceda5f776b2f302f91f8b749e2241bacaef59
Jo Young <githuborigin@gmail.com>
2023-10-29 06:14:01 +0900

    feat:비밀번호 api 주소 추가

--- ace/lib/routes/api_route.dart
+++ ace/lib/routes/api_route.dart
@@ -2,5 +2,6 @@ class ApiRoute {
   static const String baseUrl = 'https://dev.sniperfactory.com/';
   static const String login = "${baseUrl}api/auth/login";
   static const String signup = "${baseUrl}api/auth/signup";
-  
+
+  static const String forgetPassword = "${baseUrl}api/auth/forgot-password";
 }
