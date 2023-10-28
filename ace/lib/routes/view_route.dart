91a941256451cfaac30d59a43b0d8b4b222c1e98
jung Won hyeong <jeongwon264@gmail.com>
2023-10-28 20:38:07 +0900

    fix:다른 페이지들 추가

--- ace/lib/routes/view_route.dart
+++ ace/lib/routes/view_route.dart
@@ -1,4 +1,10 @@
 class ViewRoute {
   static final String mainPage = "/main";
+  static final String loginPage = "/login";
+
   static final String registrationPage = "/registration";
+  static final String changePasswordPage = "/changepassword";
+  static final String findPasswordPage = "/findpassword";
+  static final String splashPage = "/splash";
+  static final String signSuccessPage = "/signsuccess";
 }
