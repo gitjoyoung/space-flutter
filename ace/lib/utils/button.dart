3ecb882321b7192e3b448b62504c1d1faa6082d5
jung Won hyeong <jeongwon264@gmail.com>
2023-10-30 00:55:48 +0900

    fix:바뀐것 없음

--- ace/lib/utils/button.dart
+++ ace/lib/utils/button.dart
@@ -1,23 +1,87 @@
+import 'package:ace/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class AppButton {
-
   // 버튼을
   const AppButton();
 
 // 각 버튼들의 공통점 radius 설정 이 중복되고 높이값이 달리지기 때문에
 // 하나의 버튼스타일로 구성하고 높이값만 받아줍니다
   static ButtonStyle createButtonStyle(double height) {
-    return ButtonStyle( 
+    return ButtonStyle(
       shape: MaterialStateProperty.all(const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(6)))),
       minimumSize: MaterialStateProperty.all(Size.fromHeight(height)),
     );
   }
 
-// 버튼의 높이를 변수로 구분합니다
-  static final ButtonStyle xLarge = createButtonStyle(56);
-  static final ButtonStyle medium = createButtonStyle(40);
-  static final ButtonStyle small = createButtonStyle(32);
-  static final ButtonStyle xSmall = createButtonStyle(24);
+  // ElevatedButton에 특화된 스타일 생성
+  static ButtonStyle createElevatedButtonStyle(
+      double height, Color backColor, Color boradColor) {
+    return ElevatedButton.styleFrom(
+      side: BorderSide(color: boradColor),
+      backgroundColor: backColor,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      minimumSize: Size.fromHeight(height),
+    ).merge(createButtonStyle(height)); // 기존 createButtonStyle과 병합
+  }
+
+
+// 첫번째 인자값 높이 , 두번째 배경색상 , 세번째 테두리 색상
+
+// Primary
+  static final ButtonStyle xLarge= createElevatedButtonStyle(
+      56, AppColors.primaryColor, AppColors.primaryColor);
+
+  static final ButtonStyle medium= createElevatedButtonStyle(
+      40, AppColors.primaryColor, AppColors.primaryColor);
+
+  static final ButtonStyle small= createElevatedButtonStyle(
+      32, AppColors.primaryColor, AppColors.primaryColor);
+
+  static final ButtonStyle xSmall= createElevatedButtonStyle(
+      24, AppColors.primaryColor, AppColors.primaryColor);
+
+// Outline
+  static final ButtonStyle xLargeOutLine = createElevatedButtonStyle(
+      56, AppColors.prinary40, AppColors.primaryColor);
+
+  static final ButtonStyle mediumOutLine = createElevatedButtonStyle(
+      40, AppColors.prinary40, AppColors.primaryColor);
+
+  static final ButtonStyle smallOutLine = createElevatedButtonStyle(
+      32, AppColors.prinary40, AppColors.primaryColor);
+
+  static final ButtonStyle xSmallOutLine = createElevatedButtonStyle(
+      24, AppColors.prinary40, AppColors.primaryColor);
+
+//Destructiveabled
+  static final ButtonStyle xLargeDestructive = createElevatedButtonStyle(
+      56, AppColors.systemWarnin, AppColors.systemWarnin);
+
+  static final ButtonStyle mediumDestructive = createElevatedButtonStyle(
+      40, AppColors.systemWarnin, AppColors.systemWarnin);
+
+  static final ButtonStyle smallDestructive = createElevatedButtonStyle(
+      32, AppColors.systemWarnin, AppColors.systemWarnin);
+
+  static final ButtonStyle xSmallDestructive = createElevatedButtonStyle(
+      24, AppColors.systemWarnin, AppColors.systemWarnin);
+
+  //Disabled / Secondary
+  static final ButtonStyle xLargeDisabled = createElevatedButtonStyle(
+      56, AppColors.neutral5, AppColors.neutral5);
+
+  static final ButtonStyle mediumDisabled = createElevatedButtonStyle(
+      40, AppColors.neutral5, AppColors.neutral5);
+
+  static final ButtonStyle smallDisabled = createElevatedButtonStyle(
+      32, AppColors.neutral5, AppColors.neutral5);
+
+  static final ButtonStyle xSmallDisabled = createElevatedButtonStyle(
+      24, AppColors.neutral5, AppColors.neutral5);
+
+
 }
