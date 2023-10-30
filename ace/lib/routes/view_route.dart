7456d9c071fa2cc96699e59ef21102876962e1c9
jung Won hyeong <jeongwon264@gmail.com>
2023-10-30 17:12:12 +0900

    fix:충돌해결

--- ace/lib/routes/view_route.dart
+++ ace/lib/routes/view_route.dart
@@ -1,10 +1,8 @@
 class ViewRoute {
   static final String mainPage = "/main";
   static final String loginPage = "/login";
+  static final String findPassword = "/findpassword";
 
   static final String registrationPage = "/registration";
   static final String changePasswordPage = "/changepassword";
-  static final String findPasswordPage = "/findpassword";
-  static final String splashPage = "/splash";
-  static final String signSuccessPage = "/signsuccess";
 }
