25fcfabe911b745aaa37d424f9b2285596141f94
jung Won hyeong <jeongwon264@gmail.com>
2023-10-30 17:10:07 +0900

    fix:충돌 해결

--- ace/lib/controller/auth_controller.dart
+++ ace/lib/controller/auth_controller.dart
@@ -1,49 +1,56 @@
-import 'package:ace/model/user.dart';
+import 'dart:convert';
+
 import 'package:ace/routes/api_route.dart';
-import 'package:ace/views/authpage/login.dart';
-import 'package:ace/views/main_view.dart';
+import 'package:ace/utils/email_validator.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
-  final Rxn<User> _user = Rxn<User>();
-  Dio dio = Dio();
-
-  //userd의 private한 것을 사용할 수 있게 가져오기
-  User? get user => _user.value;
-
-  login(String id, String pw) async {
-    var res = await dio.post(ApiRoute.baseUrl + ApiRoute.login,
-        data: {'email': id, 'password': pw});
-    if (res.statusCode == 200) {
-      var data = Map<String, dynamic>.from(res.data);
-      _user(User.fromMap(data));
-      print(res.data);
-    }
-  }
-
-  signup() {}
-
-  logout() {
-    _user.value = null;
-  }
-
-  _handleAuthChanged(User? data) {
-    if (data != null) {
-      //메인페이지 이동
-      Get.toNamed(MainView.route);
-
-      return;
-    }
-    //로그인 페이지로 이동
-    Get.toNamed(LoginView.route);
-
-    return;
-  }
+  final Rx<String> token = Rx<String>("");
 
   @override
   void onInit() {
-    ever(_user, _handleAuthChanged);
     super.onInit();
+  }
+
+  Dio dio = Dio();
+
+  login(String email, String password) async {
+    // 이메일 비밀번호 검증
+    if (email.isEmpty || password.isEmpty) {
+      print("이메일 또는 비밀번호를 입력하지 않았습니다");
+      return "이메일 또는 비밀번호를 입력하지 않았습니다";
+    }
+
+    //이메일 양식 검증
+    if (!EmailValidator.isValid(email)) {
+      print("이메일 양식에 맞지 않습니다");
+      return "이메일 양식에 맞지 않습니다";
+    }
+
+    // 비밀번호 길이 검증
+    if (password.length < 8) {
+      print("비밀번호가 8자 미만입니다");
+      return "비밀번호가 8자 미만입니다";
+    }
+
+    String encodedPassword = base64Encode(utf8.encode(password));
+
+    // 'email': "sniperfactory@gmail.com",
+    // 'password': "MTIzMTIzMTIz",
+    var res = await dio.post(
+      ApiRoute.login,
+      data: {
+        'email': email,
+        'password': encodedPassword,
+      },
+    );
+    if (res.data['status'] == 'success') {
+      // 성공시 토큰값 업로드
+      print(res.data);
+
+      token.value = res.data['data'];
+      print(token.value);
+    }
   }
 }
