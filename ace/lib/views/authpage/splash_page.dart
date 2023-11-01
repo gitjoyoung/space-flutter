63c0749e4c9ea70e3e8760a9da53193e99eb9c7f
sungmmmm <sadlzn77@gmail.com>
2023-11-02 00:25:23 +0900

    chore: splash연결완료

--- ace/lib/views/authpage/splash_page.dart
+++ ace/lib/views/authpage/splash_page.dart
@@ -1,6 +1,5 @@
 import 'package:ace/routes/view_route.dart';
 import 'package:ace/utils/button.dart';
-import 'package:ace/utils/image_size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -8,10 +7,9 @@ import 'package:get/get.dart';
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
   static const String route = '/splashPage';
+  static const String loginPage = "/login";
   @override
   Widget build(BuildContext context) {
-    // ImageSizeConfig sizeConfig = ImageSizeConfig(context);
-
     return Scaffold(
       body: Center(
           child: Column(
@@ -24,7 +22,6 @@ class SplashPage extends StatelessWidget {
           ),
           ElevatedButton(
             onPressed: () {
-              //로그인 페이지 이동
               Get.toNamed(ViewRoute.loginPage);
             },
             child: Text('시작하기'),
