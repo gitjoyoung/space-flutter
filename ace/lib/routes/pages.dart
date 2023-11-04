2eedea7fa8b30782a138bbb8b1f8b28bafbc724a
Jo Young <githuborigin@gmail.com>
2023-11-05 00:57:56 +0900

    feat:모각 페이지 추가

--- ace/lib/routes/pages.dart
+++ ace/lib/routes/pages.dart
@@ -1,4 +1,7 @@
 
+import 'package:ace/controller/find_password_controller.dart';
+import 'package:ace/controller/login_controller.dart';
+import 'package:ace/controller/signup_controller.dart';
 import 'package:ace/routes/view_route.dart';
 import 'package:ace/views/authpage/chang_password_view.dart';
 import 'package:ace/views/authpage/find_password.dart';
@@ -7,6 +10,9 @@ import 'package:ace/views/authpage/registration_page.dart';
 import 'package:ace/views/authpage/sign_up_success.dart';
 import 'package:ace/views/authpage/splash_page.dart';
 import 'package:ace/views/main_view.dart';
+import 'package:ace/views/mogakpage/create_mogak.dart';
+import 'package:ace/views/mogakpage/detail_mogak.dart';
+import 'package:ace/views/mogakpage/mogak.dart';
 import 'package:get/get.dart';
 
 class AppPages {
@@ -38,5 +44,11 @@ class AppPages {
         page: () => const ChangePasswordView()),
     GetPage(name: ViewRoute.splashPage, page: () => const SplashPage()),
     GetPage(name: ViewRoute.signupSuccess, page: () => const SignUpSuccess()),
+    GetPage(name: ViewRoute.mogak, page: () => const Mogak()),
+    GetPage(name: ViewRoute.mogakDetail, page: () => const MogakDetail()),
+
+    GetPage(name: ViewRoute.mogakCreate, page: () => const MogakCreate()),
+
+
   ];
 }
