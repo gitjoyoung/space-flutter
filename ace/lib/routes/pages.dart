b465b0759a4a89d606beeb7b20609e27719e17aa
Jo Young <githuborigin@gmail.com>
2023-11-06 19:25:51 +0900

    feat:모각, 홈 라우터 추가

--- ace/lib/routes/pages.dart
+++ ace/lib/routes/pages.dart
@@ -1,6 +1,7 @@
-
 import 'package:ace/controller/find_password_controller.dart';
+import 'package:ace/controller/home/home_controller.dart';
 import 'package:ace/controller/login_controller.dart';
+import 'package:ace/controller/mogak/mogak_cotroller.dart';
 import 'package:ace/controller/signup_controller.dart';
 import 'package:ace/routes/view_route.dart';
 import 'package:ace/views/authpage/chang_password_view.dart';
@@ -9,9 +10,12 @@ import 'package:ace/views/authpage/login.dart';
 import 'package:ace/views/authpage/registration_page.dart';
 import 'package:ace/views/authpage/sign_up_success.dart';
 import 'package:ace/views/authpage/splash_page.dart';
+import 'package:ace/views/homepage/home.dart';
+import 'package:ace/views/homepage/home_layout.dart';
 import 'package:ace/views/main_view.dart';
 import 'package:ace/views/mogakpage/create_mogak.dart';
 import 'package:ace/views/mogakpage/detail_mogak.dart';
+import 'package:ace/views/mogakpage/list_mogak.dart';
 import 'package:ace/views/mogakpage/mogak.dart';
 import 'package:get/get.dart';
 
@@ -44,11 +48,26 @@ class AppPages {
         page: () => const ChangePasswordView()),
     GetPage(name: ViewRoute.splashPage, page: () => const SplashPage()),
     GetPage(name: ViewRoute.signupSuccess, page: () => const SignUpSuccess()),
-    GetPage(name: ViewRoute.mogak, page: () => const Mogak()),
+
+    // 홈페이지
+    GetPage(
+      name: ViewRoute.home,
+      page: () => const HomeLayOut(),
+      binding: BindingsBuilder(() {
+        Get.put(HomeController());
+      }),
+    ),
+
+    // 모각
+    GetPage(
+      name: ViewRoute.mogak,
+      page: () => const Mogak(),
+      binding: BindingsBuilder(() {
+        Get.put(MogakController());
+      }),
+    ),
     GetPage(name: ViewRoute.mogakDetail, page: () => const MogakDetail()),
-
     GetPage(name: ViewRoute.mogakCreate, page: () => const MogakCreate()),
-
-
+    GetPage(name: ViewRoute.mogakList, page: () => const MogakList()),
   ];
 }
