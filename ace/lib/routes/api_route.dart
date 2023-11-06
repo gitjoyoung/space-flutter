252a1ad78cdc633130c1df85959092006cc24c73
sungmmmm <sadlzn77@gmail.com>
2023-11-06 15:05:06 +0900

    chore: api 루트 수정

--- ace/lib/routes/api_route.dart
+++ ace/lib/routes/api_route.dart
@@ -3,15 +3,10 @@ class ApiRoute {
   static const String login = "${baseUrl}api/auth/login";
   static const String signup = "${baseUrl}api/auth/signup";
 
-  static const String forgetPassword = "${baseUrl}api/auth/forgot-password";
-
+  static const String forgetPassword = "${baseUrl}api/auth/verify-email";
+  static const String changePassword = "${baseUrl}api/auth/change-password";
 
 // 특정 모각 가져오기 생성 삭제
   static const String mogak = "${baseUrl}api/mogak/";
   static const String topMogak = "${baseUrl}api/top/mogak/";
-
-
-
-
-
 }
