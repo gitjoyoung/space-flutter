16f0a02e0f808dd5ad00806ae4100df76b16a8e9
Jo Young <githuborigin@gmail.com>
2023-11-06 19:29:29 +0900

    feat:모각 내용 위젯 생성

--- ace/lib/widgets/mogak_content.dart
+++ ace/lib/widgets/mogak_content.dart
@@ -1,71 +1,70 @@
+import 'package:ace/controller/mogak/mogak_detail_cotroller.dart';
 import 'package:ace/utils/colors.dart';
 import 'package:ace/utils/typography.dart';
+import 'package:ace/widgets/avatar_custom.dart';
 import 'package:ace/widgets/card_tag.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
-class MogakContent extends StatelessWidget {
+class MogakContent extends GetView<MogakDetailController> {
   const MogakContent({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.all(10),
       child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             ListTile(
-              onTap: () {},
               contentPadding: EdgeInsets.only(left: 0),
               title: Row(
                 children: [
-                  Container(
-                    height: 48,
-                    child: Stack(
-                      children: [
-                        SvgPicture.asset(
-                          'assets/icons/Icon50/man-h.svg',
-                          width: 33,
-                          height: 33,
-                          fit: BoxFit.cover,
-                        ),
-                        SvgPicture.asset(
-                          'assets/avatar/developer.svg ',
-                          width: 33,
-                          height: 33,
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
-                  ),
+                  AvatarCustom(height: 48, width: 43,),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text('우디' , style: AppTypograpy.button28Bold,),
+                    child: Text(
+                      '우디',
+                      style: AppTypograpy.button28Bold,
+                    ),
                   ),
                   Tag(title: '수료생'),
                 ],
               ),
-              trailing: SvgPicture.asset('assets/icons/Icon20/plus.svg'),
+              trailing: InkWell(
+                  onTap: () {},
+                  child: SvgPicture.asset('assets/icons/icon20/like.svg')),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: RichText(
-                text: TextSpan(
-                    style: AppTypograpy.tapButtonCardTitle16,
-                    children: [
-                      TextSpan(
-                          text: '[모집중] ',
-                          style: TextStyle(color: AppColors.primaryColor)),
-                      TextSpan(text: '모각코 팀을 모집합니다')
-                    ]),
+            InkWell(
+              onTap: () {
+                print('object 클릭');
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: RichText(
+                  text: TextSpan(
+                      style: AppTypograpy.tapButtonCardTitle16,
+                      children: [
+                        TextSpan(
+                            text: '[모집중] ',
+                            style: TextStyle(color: AppColors.primaryColor)),
+                        TextSpan(text: '모각코 팀을 모집합니다')
+                      ]),
+                ),
               ),
             ),
-            Text(
-              '저희는 유니티로 개발을 공부해서 게임 제작을 목표로 하고자합니다. 유니티 3d를 이용해 할 수 있는 간단한 터치 게임부터 ...',
-              style: AppTypograpy.button36Regular,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
+            InkWell(
+              onTap: () {
+                print('object 클릭');
+              },
+              child: Text(
+                '저희는 유니티로 개발을 공부해서 게임 제작을 목표로 하고자합니다. 유니티 3d를 이용해 할 수 있는 간단한 터치 게임부터 ...',
+                style: AppTypograpy.button36Regular,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
             ListTile(
               contentPadding: EdgeInsets.only(left: 0),
@@ -75,7 +74,7 @@ class MogakContent extends StatelessWidget {
                   )),
               title: Row(children: [
                 SvgPicture.asset(
-                  'assets/icons/Icon70/man-who.svg',
+                  'assets/icons/icon70/man-who.svg',
                   height: 25,
                   width: 25,
                 ),
@@ -92,9 +91,15 @@ class MogakContent extends StatelessWidget {
                     TextSpan(text: '/4'),
                     TextSpan(text: '참여')
                   ]),
-                )
+                ),
               ]),
             ),
+            Row(
+              children: [
+                Tag(title: 'title'),
+                Tag(title: 'title'),
+              ],
+            )
           ]),
     );
   }
