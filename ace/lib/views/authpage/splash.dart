ae76fecde08300f18ca0697f32bbf4db1b74c05f
jung Won hyeong <jeongwon264@gmail.com>
2023-11-07 17:29:40 +0900

    fix:필요없는 코드 수정

--- ace/lib/views/authpage/splash.dart
+++ ace/lib/views/authpage/splash.dart
@@ -6,8 +6,6 @@ import 'package:get/get.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
-  static const String route = '/splashPage';
-  static const String loginPage = "/login";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
