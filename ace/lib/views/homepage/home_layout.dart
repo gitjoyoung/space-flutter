9dbfba65f69e101fdd206605f0777ba3951c3599
sungmmmm <sadlzn77@gmail.com>
2023-11-07 22:13:07 +0900

    fix : 홈페이지 크기 수정

--- ace/lib/views/homepage/home_layout.dart
+++ ace/lib/views/homepage/home_layout.dart
@@ -23,7 +23,7 @@ class HomeLayOut extends GetView<HomeController> {
     return Scaffold(
       appBar: SpaceAppBar(),
       bottomNavigationBar: Container(
-        height: 60,
+        height: 80,
         child: Obx(
           () => BottomNavigationBar(
             type: BottomNavigationBarType.fixed,
