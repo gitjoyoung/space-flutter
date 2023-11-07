a5df0c7ff5f92e046abaaf8916d1c274c47e6b58
Jo Young <githuborigin@gmail.com>
2023-11-07 16:55:36 +0900

    feat:모각 컨텐츠 컨트롤러로 각항목에 내용받아오기

--- ace/lib/widgets/mogak_content.dart
+++ ace/lib/widgets/mogak_content.dart
@@ -1,15 +1,20 @@
 import 'package:ace/controller/mogak/mogak_detail_cotroller.dart';
+import 'package:ace/models/mogak/mogak_model.dart';
+import 'package:ace/routes/view_route.dart';
 import 'package:ace/utils/colors.dart';
 import 'package:ace/utils/typography.dart';
 import 'package:ace/widgets/avatar_custom.dart';
 import 'package:ace/widgets/card_tag.dart';
+import 'package:ace/widgets/tag_row.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
 class MogakContent extends GetView<MogakDetailController> {
-  const MogakContent({super.key});
+  const MogakContent({this.data, this.maxLength, super.key});
 
+  final AllMogakModel? data;
+  final int? maxLength;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,11 +27,14 @@ class MogakContent extends GetView<MogakDetailController> {
               contentPadding: EdgeInsets.only(left: 0),
               title: Row(
                 children: [
-                  AvatarCustom(height: 48, width: 43,),
+                  AvatarCustom(
+                    height: 48,
+                    width: 43,
+                  ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
                     child: Text(
-                      '우디',
+                      data?.author?.nickname ?? "",
                       style: AppTypograpy.button28Bold,
                     ),
                   ),
@@ -39,7 +47,8 @@ class MogakContent extends GetView<MogakDetailController> {
             ),
             InkWell(
               onTap: () {
-                print('object 클릭');
+                Get.toNamed(ViewRoute.mogakDetail, arguments: data?.id);
+                print('${data?.id}  제목 클릭');
               },
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8),
@@ -48,27 +57,36 @@ class MogakContent extends GetView<MogakDetailController> {
                       style: AppTypograpy.tapButtonCardTitle16,
                       children: [
                         TextSpan(
-                            text: '[모집중] ',
+                            text: RegExp(r'\[.*?\]')
+                                .firstMatch(data?.title ?? "")
+                                ?.group(0),
                             style: TextStyle(color: AppColors.primaryColor)),
-                        TextSpan(text: '모각코 팀을 모집합니다')
+                        TextSpan(
+                            text: (data?.title ?? "")
+                                .replaceAll(RegExp(r'\[.*?\]'), ''),
+                            style: TextStyle(color: Colors.black)),
                       ]),
                 ),
               ),
             ),
             InkWell(
               onTap: () {
-                print('object 클릭');
+                Get.toNamed(ViewRoute.mogakDetail, arguments: data?.id);
+                print('${data?.id}   내용 클릭');
               },
               child: Text(
-                '저희는 유니티로 개발을 공부해서 게임 제작을 목표로 하고자합니다. 유니티 3d를 이용해 할 수 있는 간단한 터치 게임부터 ...',
+                data?.content ?? "",
                 style: AppTypograpy.button36Regular,
-                maxLines: 2,
+                maxLines: maxLength ?? 2,
                 overflow: TextOverflow.ellipsis,
               ),
             ),
             ListTile(
               contentPadding: EdgeInsets.only(left: 0),
-              trailing: Text('2023.09.04',
+              trailing: Text(
+                  data?.createdAt == null
+                      ? ""
+                      : "${data!.createdAt.year}.${data!.createdAt.month.toString().padLeft(2, '0')}.${data!.createdAt.day.toString().padLeft(2, '0')}",
                   style: AppTypograpy.cardBody.copyWith(
                     color: AppColors.neutral40,
                   )),
@@ -82,24 +100,30 @@ class MogakContent extends GetView<MogakDetailController> {
                   width: 6,
                 ),
                 RichText(
-                  text: TextSpan(children: [
+                  text: TextSpan(style: AppTypograpy.cardBody, children: [
                     TextSpan(
-                        text: '3',
+                        text: data?.appliedProfiles?.length.toString() ?? "0",
                         style: TextStyle(
                             color: AppColors.primaryColor,
                             fontWeight: FontWeight.bold)),
-                    TextSpan(text: '/4'),
-                    TextSpan(text: '참여')
+                    TextSpan(
+                        text: '/',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold)),
+                    TextSpan(
+                        text: data?.maxMember.toString() ?? "",
+                        style: TextStyle(color: Colors.black)),
+                    TextSpan(
+                        text: ' 참여', style: TextStyle(color: Colors.black)),
                   ]),
                 ),
               ]),
             ),
-            Row(
-              children: [
-                Tag(title: 'title'),
-                Tag(title: 'title'),
-              ],
-            )
+            TagsRow(
+              tagsString: (data?.hashtag?.trim().isEmpty ?? true)
+                  ? "#태그없음"
+                  : data!.hashtag!,
+            ),
           ]),
     );
   }
