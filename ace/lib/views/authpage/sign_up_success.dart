1af381fffac53c2f755a3cef0c2d5e3eff5bf41a
wh5905 <jeongwon264@gmail.com>
2023-11-08 19:55:10 +0900

    fix:토큰받기 및  페이지연결

--- ace/lib/views/authpage/sign_up_success.dart
+++ ace/lib/views/authpage/sign_up_success.dart
@@ -1,52 +1,82 @@
-import 'package:ace/controller/avatar_controller.dart';
-import 'package:ace/routes/pages.dart';
 import 'package:ace/routes/view_route.dart';
 import 'package:ace/utils/button.dart';
 import 'package:ace/utils/image_size_config.dart';
 
 import 'package:ace/utils/typography.dart';
+import 'package:ace/views/homepage/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
 void main() {
-  runApp(GetMaterialApp(
-    home: const SignUpSuccess(),
-    getPages: AppPages.pages,
-  ));
+  runApp(GetMaterialApp(home: const SignUpSuccess(), getPages: [
+    GetPage(name: ViewRoute.profile, page: () => const Profile()),
+  ]));
 }
 
-class SignUpSuccess extends StatelessWidget {
+class SignUpSuccess extends GetView {
   const SignUpSuccess({super.key});
 
   @override
   Widget build(BuildContext context) {
-    ImageSizeConfig sizeConfig = ImageSizeConfig(context);
+    return Builder(
+      builder: (BuildContext innerContext) {
+        // 여기서 innerContext는 Builder에 의해 생성되었으며, 이 context는 사용 가능합니다.
+        ImageSizeConfig sizeConfig = ImageSizeConfig(innerContext);
 
-    return Scaffold(
-      body: Center(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              'assets/login/signupsuccess.svg',
-              height: sizeConfig.imageHeight,
-              width: sizeConfig.imageWidth,
-            ),
-            SizedBox(height: 100),
-            ElevatedButton(
-              onPressed: () {
-                Get.toNamed(ViewRoute.profile);
-              },
-              child: Text('프로필 작성하기', style: AppTypograpy.button36Regular),
-              style: AppButton.xLarge,
-            )
-          ],
-        ),
-      )),
+        return Container(
+          child: Scaffold(
+            body: Center(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'assets/login/signupsuccess.svg',
+                    height: sizeConfig.imageHeight,
+                    width: sizeConfig.imageWidth,
+                  ),
+                  SizedBox(height: 100),
+                  ElevatedButton(
+                    onPressed: () {
+                      Get.toNamed(ViewRoute.profile);
+                    },
+                    child:
+                        Text('프로필 작성하기', style: AppTypograpy.button36Regular),
+                    style: AppButton.xLarge,
+                  )
+                ],
+              ),
+            )),
+          ),
+        );
+      },
+      // child: Scaffold(
+
+      //   body: Center(
+      //       child: Padding(
+      //     padding: const EdgeInsets.symmetric(horizontal: 16),
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         SvgPicture.asset(
+      //           'assets/login/signupsuccess.svg',
+      //           height: sizeConfig.imageHeight,
+      //           width: sizeConfig.imageWidth,
+      //         ),
+      //         SizedBox(height: 100),
+      //         ElevatedButton(
+      //           onPressed: () {
+      //             Get.toNamed(ViewRoute.profile);
+      //           },
+      //           child: Text('프로필 작성하기', style: AppTypograpy.button36Regular),
+      //           style: AppButton.xLarge,
+      //         )
+      //       ],
+      //     ),
+      //   )),
     );
   }
 }
