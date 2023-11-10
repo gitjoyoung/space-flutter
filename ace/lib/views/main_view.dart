6fcdba0ee7f0f73df09d4ceff7f8ef81ff85dc9f
sungmmmm <sadlzn77@gmail.com>
2023-11-10 12:18:09 +0900

    fix:  오류 수정

--- ace/lib/views/main_view.dart
+++ ace/lib/views/main_view.dart
@@ -1,4 +1,5 @@
 import 'package:ace/routes/view_route.dart';
+import 'package:ace/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,7 +12,34 @@ class MainView extends GetView {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Text('MainView'),
-    ));
+            child: Padding(
+                padding: const EdgeInsets.only(top: 16, left: 10, right: 10),
+                child: SingleChildScrollView(
+                    child: Column(children: [
+                  Container(
+                    height: 48,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Icon(Icons.search),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: AppColors.strokeLine05, width: 2.0),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: AppColors.primaryColor, width: 2.0),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        hintText: '내용 검색하기',
+                      ),
+                    ),
+                  ),
+                ])))));
   }
 }
