c261c0a8ef32c04d88fa29afbb9092ea2241f27a
Jo Young <githuborigin@gmail.com>
2023-11-15 18:17:28 +0900

    feat:말풍선

--- ace/lib/widgets/talk/talk_content.dart
+++ ace/lib/widgets/talk/talk_content.dart
@@ -5,6 +5,8 @@ import 'package:ace/utils/colors.dart';
 import 'package:ace/utils/typography.dart';
 import 'package:ace/widgets/badge_avatar_custom.dart';
 import 'package:ace/widgets/comment_Icon.dart';
+import 'package:ace/widgets/talk_dox.dart';
+import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -42,10 +44,11 @@ class CustomTalkWidget extends GetView<TalkController> {
             children: [
               SizedBox(
                 width: double.infinity,
-                child: Card(
-                  margin: EdgeInsets.only(
-                    left: 10,
-                  ),
+                child: Bubble(
+                  borderColor: AppColors.neutral10,
+                  elevation: 0,
+                  stick: true,
+                  nip: BubbleNip.leftBottom,
                   child: Padding(
                     padding: const EdgeInsets.only(
                         left: 16, top: 16, bottom: 18, right: 16),
