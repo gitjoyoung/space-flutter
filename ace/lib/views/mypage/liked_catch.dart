9bb49eba0c5cade0ef9ecfd7f2dde3bf292b6aee
sungmmmm <sadlzn77@gmail.com>
2023-11-16 10:53:47 +0900

    feat : 좋아요 한 캐치업

---
+++ ace/lib/views/mypage/liked_catch.dart
@@ -0,0 +1,17 @@
+import 'package:ace/widgets/space_appbar.dart';
+import 'package:flutter/material.dart';
+
+class LikedCatch extends StatelessWidget {
+  const LikedCatch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: SpaceAppBar(),
+      body: Center(
+        child: Text('liked catch'),
+      ),
+    ));
+  }
+}
