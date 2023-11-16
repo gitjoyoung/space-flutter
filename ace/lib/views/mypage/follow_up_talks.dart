3f6445cad15b171e12fc90aa1298207c65ca8642
sungmmmm <sadlzn77@gmail.com>
2023-11-16 18:11:52 +0900

    feat : 내가 쓴 이어달린 톡 파일 추가

---
+++ ace/lib/views/mypage/follow_up_talks.dart
@@ -0,0 +1,17 @@
+import 'package:ace/widgets/common/space_appbar.dart';
+import 'package:flutter/material.dart';
+
+class FollowUpTalks extends StatelessWidget {
+  const FollowUpTalks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: SpaceAppBar(),
+      body: Center(
+        child: Text('follow_up_talks'),
+      ),
+    ));
+  }
+}
